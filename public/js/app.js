2fb75f7e7574de352e35671cb880adf16503c2c8
Luzzi Charly <luzzi.charly@gmail.com>
2020-04-03 03:24:45 +0200

    Front new/delete task

--- public/js/app.js
+++ public/js/app.js
@@ -1993,6 +1993,15 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
 /* harmony default export */ __webpack_exports__["default"] = ({
   name: 'TasksComponent',
   data: function data() {
@@ -2060,6 +2069,16 @@ __webpack_require__.r(__webpack_exports__);
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_ToDoList_vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../components/ToDoList.vue */ "./resources/js/components/ToDoList.vue");
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -38215,9 +38234,44 @@ var render = function() {
       "ul",
       {},
       _vm._l(_vm.tasks, function(task) {
-        return _c("li", { key: task.id, staticClass: "mb-4 bg-gray-200" }, [
-          _vm._v("\n            " + _vm._s(task.name) + "\n        ")
-        ])
+        return _c(
+          "li",
+          {
+            key: task.id,
+            staticClass: "mb-4 p-3 rounded bg-gray-200 flex justify-between"
+          },
+          [
+            _c("p", { staticClass: "m-0" }, [
+              _vm._v(
+                "\n                " + _vm._s(task.name) + "\n            "
+              )
+            ]),
+            _vm._v(" "),
+            _c("div", {}, [
+              _c(
+                "svg",
+                {
+                  staticClass: "fill-current h-6 w-6 text-red-500",
+                  attrs: {
+                    role: "button",
+                    xmlns: "http://www.w3.org/2000/svg",
+                    viewBox: "0 0 20 20"
+                  }
+                },
+                [
+                  _c("title", [_vm._v("Close")]),
+                  _vm._v(" "),
+                  _c("path", {
+                    attrs: {
+                      d:
+                        "M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+                    }
+                  })
+                ]
+              )
+            ])
+          ]
+        )
       }),
       0
     )
@@ -38284,12 +38338,37 @@ var render = function() {
     [
       _c("h1", { staticClass: "text-3xl" }, [_vm._v("ToDo List")]),
       _vm._v(" "),
+      _vm._m(0),
+      _vm._v(" "),
       _c("ToDoList")
     ],
     1
   )
 }
-var staticRenderFns = []
+var staticRenderFns = [
+  function() {
+    var _vm = this
+    var _h = _vm.$createElement
+    var _c = _vm._self._c || _h
+    return _c("div", { staticClass: "flex w-full mb-3" }, [
+      _c("input", {
+        staticClass:
+          "shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline",
+        attrs: { id: "username", type: "text", placeholder: "New task..." }
+      }),
+      _vm._v(" "),
+      _c(
+        "button",
+        {
+          staticClass:
+            "ml-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline",
+          attrs: { type: "button" }
+        },
+        [_vm._v("\n            Send\n        ")]
+      )
+    ])
+  }
+]
 render._withStripped = true
 
 
--- resources/js/components/TodoList.vue
+++ resources/js/components/TodoList.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="tasksComponent">
         <ul class="">
-            <li class="mb-4 bg-gray-200"
+            <li class="mb-4 p-3 rounded bg-gray-200 flex justify-between"
                 v-for="task in tasks"
                 :key="task.id">
-                {{task.name}}
+                <p class="m-0">
+                    {{task.name}}
+                </p>
+                <div class="">
+                    <svg class="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                        <title>Close</title>
+                        <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/>
+                    </svg>
+                </div>
+
             </li>
         </ul>
     </div>
